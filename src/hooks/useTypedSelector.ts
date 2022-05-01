53acab653e533bd9875cc75b3e39f4615460b87f
Mikayel <abachyanm@gmail.com>
2022-05-01 13:18:38 +0400

    first commit

---
+++ src/hooks/useTypedSelector.ts
@@ -0,0 +1,8 @@
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {RootState} from "../store/reducers";
+
+
+
+//export const useTypeSelector = TypedUseSelectorHook<RootState> = useSelector
+
+export const useTypeSelector: TypedUseSelectorHook<RootState> = useSelector
